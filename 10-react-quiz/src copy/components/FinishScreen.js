0f410fc736c30e4ddcffe8aff407589cf4d492bb
ZaidHweidi <zaid.hwiedi1@gmail.com>
2026-08-18 19:16:50 +0300

    Finished WorldWise app

---
+++ 10-react-quiz/src copy/components/FinishScreen.js
@@ -0,0 +1,13 @@
+function FinishScreen({points, maxPoints, highscore, dispatch}){
+
+    const percentage = (points/maxPoints) * 100;
+
+    return(
+        <>
+        <p className="result">You scored <strong>{points}</strong> out of {maxPoints} ({Math.ceil(percentage)})%</p>
+        <p className="highscore">(Highscore: {highscore} points)</p>
+        </>
+    );
+}
+
+export default FinishScreen;
